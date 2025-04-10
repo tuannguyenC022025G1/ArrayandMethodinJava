6eddac5b673577dd2c7efc8af772437b89e2d316
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-04-10 10:47:46 +0700

    .

---
+++ findMIN/src/FindMinimumValue.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FindMinimumValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        int min = findMinValue(array);
+        System.out.println("The smallest value in the array is: " + min);
+    }
+
+
+    public static int findMinValue(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
